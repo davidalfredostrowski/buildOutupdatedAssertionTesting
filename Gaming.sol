ef3eb5339e270bf80b416a13c85d8840f43b7c66
davidalfredostrowski <davidaostrowski@gmail.com>
2024-07-11 18:52:58 -0400

    Update Gaming.sol

--- Gaming.sol
+++ Gaming.sol
@@ -64,3 +64,152 @@ contract Gaming {
         } else if (guess == false) {
             if (number > display) {
            
+              }
+            if (number < display) {
+                return true;
+            }
+        }
+    }
+
+    event RoundComplete(uint wager, uint playerNumber, uint mysteryNumber, bool guess, string result);
+    function winOrLose(uint display, bool guess) external payable returns (bool, uint) {
+    /* function winOrLose(uint display, bool guess, uint wager) external payable returns (bool, uint) { */
+        /* Use true for a higher guess, false for a lower guess */
+        require(online == true, "The game is not online");
+        require(msg.sender.balance > msg.value, "Insufficient funds");
+        uint mysteryNumber_ = mysteryNumber();
+        bool isWinner = determineWinner(mysteryNumber_, display, guess);
+        Player storage player = players[msg.sender];
+        if (isWinner == true) {
+            /* Player won */
+            player.wins += 1;
+            wins += 1;
+
+
+
+
+
+
+
+   event RoundComplete(uint wager, uint playerNumber, uint mysteryNumber, bool guess, string result);
+    function winOrLose(uint display, bool guess) external payable returns (bool, uint) {
+    /* function winOrLose(uint display, bool guess, uint wager) external payable returns (bool, uint) { */
+        /* Use true for a higher guess, false for a lower guess */
+        require(online == true, "The game is not online");
+        require(msg.sender.balance > msg.value, "Insufficient funds");
+        uint mysteryNumber_ = mysteryNumber();
+        bool isWinner = determineWinner(mysteryNumber_, display, guess);
+        Player storage player = players[msg.sender];
+        if (isWinner == true) {
+            /* Player won */
+            player.wins += 1;
+            wins += 1;
+            payable(msg.sender).transfer(msg.value * 2);
+            /* msg.sender.transfer(wager * 2); */
+            emit RoundComplete(msg.value, display, mysteryNumber_, guess, "won");
+            return (true, mysteryNumber_);
+        } else if (isWinner == false) {
+            /* Player lost */
+            player.losses += 1;
+            losses += 1;
+            emit RoundComplete(msg.value, display, mysteryNumber_, guess, "lost");
+            return (false, mysteryNumber_);
+        }
+    }
+
+
+
+   event RoundComplete4(uint wager, uint playerNumber, uint mysteryNumber, bool guess, string result);
+    function winOrLose4(uint display, bool guess) external payable returns (bool, uint) {
+        /* Use true for a higher guess, false for a lower guess */
+        require(online == true, "The game is not online");
+        require(msg.sender.balance > msg.value, "Insufficient funds");
+        uint mysteryNumber_ = mysteryNumber();
+        bool isWinner = determineWinner(mysteryNumber_, display, guess);
+        Player storage player = players[msg.sender];
+        if (isWinner == true) {
+            /* Player won */
+            player.wins += 1;
+            wins += 1;
+            payable(msg.sender).transfer(msg.value * 2);
+            emit RoundComplete(msg.value, display, mysteryNumber_, guess, "won");
+            return (true, mysteryNumber_);
+        } else if (isWinner == false) {
+            /* Player lost */
+            player.losses += 1;
+            losses += 1;
+            emit RoundComplete4(msg.value, display, mysteryNumber_, guess, "lost");
+            return (false, mysteryNumber_);
+        }
+    }
+  function winOrLose3(uint display, bool guess) external payable returns (bool, uint) {
+        /* Use true for a higher guess, false for a lower guess */
+        require(online == true, "The game is not online");
+        require(msg.sender.balance > msg.value, "Insufficient funds");
+        uint mysteryNumber_ = mysteryNumber();
+        bool isWinner = determineWinner(mysteryNumber_, display, guess);
+        Player storage player = players[msg.sender];
+        if (isWinner == true) {
+            /* Player won */
+            player.wins += 1;
+            wins += 1;
+            payable(msg.sender).transfer(msg.value * 2);
+            emit RoundComplete(msg.value, display, mysteryNumber_, guess, "won");
+            return (true, mysteryNumber_);
+        } else if (isWinner == false) {
+            /* Player lost */
+            player.losses += 1;
+            losses += 1;
+            emit RoundComplete(msg.value, display, mysteryNumber_, guess, "lost");
+            return (false, mysteryNumber_);
+        }
+    }
+   function winOrLose2() external payable returns ( uint) {
+        /* Use true for a higher guess, false for a lower guess */
+        require(online == true, "The game is not online");
+        require(msg.sender.balance > msg.value, "Insufficient funds");
+        uint mysteryNumber_ = mysteryNumber();
+        return (mysteryNumber_);
+
+
+     }
+
+
+    function withdrawFunds() public isOwner {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    function fundGame() public isOwner payable {
+        emit GameFunded(msg.sender, msg.value);
+    }
+
+//    fallback() external payable {
+//    }
+
+}
+   function winOrLose2() external payable returns ( uint) {
+        /* Use true for a higher guess, false for a lower guess */
+        require(online == true, "The game is not online");
+        require(msg.sender.balance > msg.value, "Insufficient funds");
+        uint mysteryNumber_ = mysteryNumber();
+        return (mysteryNumber_);
+
+
+     }
+
+
+    function withdrawFunds() public isOwner {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    function fundGame() public isOwner payable {
+        emit GameFunded(msg.sender, msg.value);
+    }
+
+//    fallback() external payable {
+//    }
+
+}
+
+
+
